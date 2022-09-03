0a35bd80017afcf0df8b719799824266c2534881
Dawid Badora <dawidbadora@gmail.com>
2022-09-04 01:36:52 +0200

    Interface

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+
 public class IDandPasswords {
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+    IDandPasswords(){
+
+        logininfo.put("mark123","LAL123");
+        logininfo.put("daw123","car123");
+        logininfo.put("camile123","plane123");
+    }
+
+    protected HashMap getLoginInfo(){
+        return logininfo;
+    }
 }
